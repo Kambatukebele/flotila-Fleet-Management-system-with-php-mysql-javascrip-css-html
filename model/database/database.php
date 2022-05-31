3e80db2b312ead2f05ff37b736f1a723aece4753
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-05-31 08:52:47 +0200

    add session start

--- model/database/database.php
+++ model/database/database.php
@@ -8,6 +8,8 @@
     //CONNECT TO THE DATABASE USING PD0
     try
     {
+        session_start();
+        
         $conn = new PDO("mysql:host=$server_host;dbname=$db_name", $user_name, $password);
 
         // SET THE PDO ERROR MODE TO EXCEPTION
