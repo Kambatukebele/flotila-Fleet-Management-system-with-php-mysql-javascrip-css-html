ff1091f9d9a9a469fa8e351df96a9bff62797d15
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-06-09 09:20:02 +0200

    controller new driver

--- controller/controller_list_registered_new_driver.php
+++ controller/controller_list_registered_new_driver.php
@@ -5,21 +5,10 @@
     //FETCH DATA FROM registration_new_driver
 
     $sql = "SELECT * FROM registration_new_driver";
-    $stmt = $conn->query($sql);
-    
+    $stmt = $conn->query($sql);  
 
     //SET THE RESULTING ARRAY TO ASSOCIATIVE
 
     $result = $stmt->fetchAll();
-    foreach($result as  $list => $newDriver){
-        $driverFirstName = $newDriver["first_name"];
-        $driverlastName = $newDriver["last_name"];
-        $driverEmail = $newDriver["email"];
-        $driverPhoneNumber = $newDriver["phone_number"];
-        $driverTransportation = $newDriver["transportation"];
-        $driverCity = $newDriver["city"];
-        $driverTextPlace= $newDriver["textplace"];
-    }
-
-    $conn = null;
+  
     
