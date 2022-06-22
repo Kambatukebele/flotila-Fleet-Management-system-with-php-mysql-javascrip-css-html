7ee11c7c3f03137bd025c268d245d13b53e1003a
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-06-22 21:15:56 +0200

    all driver

--- view/all-driver.php
+++ view/all-driver.php
@@ -1,4 +1,8 @@
-<?php include '../controller/controller_all-driver.php'; ?>
+<?php 
+    include_once '../controller/controller_all-driver.php';
+   
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <?php include "../model/includes/head_drivers/head_drivers.php"; ?>
@@ -7,18 +11,26 @@
     <!-- ========== TABLE USERS========== -->
     <div class="driver_list">
         <div class="driver_list_all">
-            <table class="drivers">
+            <table class="drivers" style="width: 1300px;">
                 <tr>
                   <th>Jméno</th>
                   <th>Příjmení</th>
+                  <th>Bank_Account</th>
+                  <th>Registration</th>
+                  <th>Approved</th>
+                  <th>Bag?</th>
+                  <th>Signed</th>
+                  <th>Starting</th>
                   <th>City</th>
                   <th>Type</th>
                   <th>Date</th>
-                  <th>completed?</th>
+                  
                   <th>Action</th>
                 </tr>
                 <?php
+                    
                    foreach($stmt as $all_driver){
+
                          $id = $all_driver['id'];
                          $all_driver['first_name'];
                          $all_driver['last_name'];
@@ -35,16 +47,20 @@
                          $all_driver['starting_date'];
                          $all_driver['return_deposit'];
                          $all_driver['registered_to_bolt'];
-                                               
-                       
+                  
                         echo "<tr>";
                         echo "<td>".$all_driver['first_name']."</td>";
                         echo "<td>".$all_driver['last_name']."</td>";
-                        echo     "<td>".$all_driver['city']."</td>";
-                       echo      "<td>".$all_driver['kind-of-registration']."</td>";
-                        echo    "<td>".$all_driver['date_of_submission']."</td>";
-                       echo     "<td>"."yes"."</td>";
-                        echo    "<td>";
+                        echo "<td>".$all_driver['bank-account']."</td>";
+                        echo "<td>".$all_driver['registered_to_bolt']."</td>";
+                        echo "<td>".$all_driver['approved']."</td>";
+                        echo "<td>".$all_driver['pick_the_bag']."</td>";
+                        echo "<td>".$all_driver['signed']."</td>";
+                        echo "<td>".$all_driver['starting_date']."</td>";
+                        echo "<td>".$all_driver['city']."</td>";
+                       echo  "<td>".$all_driver['kind-of-registration']."</td>";
+                        echo "<td>".$all_driver['date_of_submission']."</td>";
+                        echo "<td>";
                         echo '<button class="upd">'.'<a href="updatedriver.php?updateid='.$id.'">'.'Update'.'</a>'."</button>";
                          echo  '<button class="del">'.'<a href="deletedriver.php=">'.'Delete'.'</a>'."</button>";
                          echo   "</td>";
@@ -53,7 +69,9 @@
                 ?>
               </table>
         </div>
+       
     </div>
+   
 </body>
 </html>
 
