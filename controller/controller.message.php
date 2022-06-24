ab3a051bc53c3f51849ecdb1a5a2741a1ed0e87e
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-06-24 09:23:26 +0200

    message

---
+++ controller/controller.message.php
@@ -0,0 +1,6 @@
+<?php 
+   //LINK THE DATABASE CONNECTION
+   include 'database/database.php';
+      //SELECT ALL DATA FROM registration driver
+    $sql = $conn->query("SELECT * FROM registration_new_driver WHERE id = $id");
+    $stmt = $sql->fetchAll();
