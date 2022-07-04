0fd5541646969a086f3c78df6dd8f82ef48bfa1c
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-04 20:12:41 +0200

    Update controller_all-driver.php

--- controller/controller_all-driver.php
+++ controller/controller_all-driver.php
@@ -3,5 +3,5 @@
     include 'security/security.php';
 
     // DISPLAY THE LIST OF ALL DRIVERS  
-    $sql = $conn->query("SELECT * FROM registration_new_driver");
+    $sql = $conn->query("SELECT * FROM `registration_new_driver` ORDER BY `id` DESC");
     $stmt = $sql->fetchAll();
