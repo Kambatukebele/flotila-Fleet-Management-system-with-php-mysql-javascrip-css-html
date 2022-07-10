e32dd40438a04a372d145924d1cdbcb4bf444354
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-10 19:45:56 +0200

    Update controller_payment_details.php

--- controller/controller_payment_details.php
+++ controller/controller_payment_details.php
@@ -28,6 +28,39 @@
 
 
     // RETRIVE DATA FROM PAYMENT TABLE
-    $paymentStmt = $conn->prepare("SELECT * FROM `payments` WHERE paymentID = ?");
-    $paymentStmt->execute(array($idDriver));
-    $paymentStmtResult = $paymentStmt->fetchAll(PDO::FETCH_ASSOC);
+   //  $paymentStmt = $conn->prepare("SELECT * FROM `payments` WHERE paymentID = ?");
+   //  $paymentStmt->execute(array($idDriver));
+   //  $paymentStmtResult = $paymentStmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+   //PAGINATION
+
+    //CACULATE TOTAL NUMBER OF PAGES
+    $perPage = 10;
+    $sql = $conn->prepare("SELECT COUNT(*) FROM `payments`");
+    $sql->execute();
+    $stmt = $sql->fetchColumn();
+    $totalPages = ceil($stmt / $perPage);
+
+    //echo "there are $totalPages drivers in total"."<br>";
+
+    //GET ENTRY FOR THE CURRENT PAGE
+    //USE $_GET["PAGE"]
+
+    $pageNow = isset($_GET['page']) ? $_GET['page'] : 1;
+
+   // echo "it is $pageNow now!";
+
+    //SQL FETCH
+    //$x is the offset
+    //$y is the number of rows
+    //E.G. LIMIT 0,10 - GET FIRST ENTRY TO 10TH
+    //LIMIT 10,10 -GET 11TH ENTRY TO 20TH
+    $x = ($pageNow - 1) * $perPage;
+    $y = $perPage;
+    $sqlSelect = $conn->prepare("SELECT * FROM `payments` WHERE paymentID = ? ORDER BY `id` LIMIT $x, $y ");
+    $sqlSelect->execute(array($idDriver));
+    $drivers = $sqlSelect->fetchAll();
+   //  $stmt = $conn->prepare($sqlSelect);
+   //  $stmt->execute();
+   //  $drivers = $stmt->fetchAll();
