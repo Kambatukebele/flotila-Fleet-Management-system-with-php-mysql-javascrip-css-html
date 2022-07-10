a8e95c06f5ac06bb2fc74a5e30d9b882ac1e53a5
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-10 19:28:07 +0200

    Update payment.php

--- view/payment.php
+++ view/payment.php
@@ -9,6 +9,12 @@
     <link rel="stylesheet" href="../model/public/css/style.css">
     <link rel="stylesheet" href="../model/public/css/reset.css">
     <title>card driver</title>
+    <style>
+      body{
+        background-color: rgba(128, 128, 128, 0.26);
+        font-family: 'Poppins', sans-serif;
+      }
+    </style>
 </head>
 <body>
 
@@ -18,8 +24,9 @@
         <div class="driver_list_all">
         <button class="upd" style="background-color:green; border:none; padding: 7px 9px;"><a href="dashboard.php" style="color: white; text-decoration:none;">Back to dashboard</a></button>
             <br><br>
-            <table class="drivers">
-                <tr>
+            <table class="drivers" style="width:1000px;-webkit-box-shadow: 5px 5px 15px 5px #A8A8A8; 
+box-shadow: 5px 5px 15px 5px #A8A8A8;">
+                <tr style="background-color:#27B973;">
                   <th>Jméno</th>
                   <th>Příjmení</th>
                   <th>Email</th>
@@ -29,7 +36,7 @@
                   <th>Action</th>
                 </tr>
                 <?php
-                     foreach($stmt->fetchAll() as $driver=>$row){
+                     foreach($drivers as $row){
                        $idDriver =  htmlspecialchars($row['id']);
                        $firstNameD =  htmlspecialchars($row['first_name']);
                        $lastNameD =  htmlspecialchars($row['last_name']);
@@ -49,7 +56,7 @@
                           '<td>'.$typeOfD.'</td>'.
                           '<td>'.
                             '<button class="del" style="background-color:red;padding: 3px 7px;margin-right: 9px;">'.'<a style="color:white;" href="payment_details.php?detailsId='.$idDriver.'">'.'Details'.'</a>'.'</button>'.
-                            '<button class="upd">'.'<a href="card_payment_driver.php?payId='.$idDriver.'">'.'Payment'.'</a>'.'</button>'.                    
+                            '<button class="upd" style="background-color:#27B973;padding: 3px 7px;margin-right: 9px;>'.'<a href="card_payment_driver.php?payId='.$idDriver.'">'.'Payment'.'</a>'.'</button>'.                    
                           '</td>'.
                         '</tr>';
                    }
@@ -58,6 +65,13 @@
               </table>
         </div>
     </div>  
+    <div class="pagination" style="display:flex; justify-content:center; background-color: rgba(128, 128, 128, 0.26);">
+            <?php
+                for($i=1; $i<=$totalPages; $i++){
+                    printf("<a style='padding:5px;' href='payment.php?page=%u'>%u</a>", $i, $i);
+                }
+            ?>
+    </div>
 
     </body>
     </html>
