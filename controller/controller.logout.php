a943d2288b241535fb3dd173126f60c95318647e
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-11 08:43:41 +0200

    Update controller.logout.php

--- controller/controller.logout.php
+++ controller/controller.logout.php
@@ -2,6 +2,9 @@
     require_once 'database/database.php';
     require_once 'security/security.php';
 
-    session_start();
+   if(isset(session_start())){
     session_unset();
     session_destroy();
+    header("location:login.php");
+   }
+    
