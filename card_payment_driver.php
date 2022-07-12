fdc152d7d8e198f2b818aef92fae6f348dc40331
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-12 07:01:33 +0200

    Update card_payment_driver.php

--- card_payment_driver.php
+++ card_payment_driver.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../model/public/css/card_driver.css">
-    <link rel="stylesheet" href="../model/public/css/style.css">
-    <link rel="stylesheet" href="../model/public/css/reset.css">
+    <link rel="stylesheet" href="model/public/css/card_driver.css">
+    <link rel="stylesheet" href="model/public/css/style.css">
+    <link rel="stylesheet" href="model/public/css/reset.css">
     <title>card driver</title>
 </head>
 <body>
