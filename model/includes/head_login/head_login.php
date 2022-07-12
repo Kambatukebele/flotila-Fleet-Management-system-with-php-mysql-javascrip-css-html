95a2c0e5adb5a30d3339c64139a4226ec1aa98e4
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-12 06:38:38 +0200

    Update head_login.php

--- model/includes/head_login/head_login.php
+++ model/includes/head_login/head_login.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- ========== LINK TO CSS ========== -->
-    <link rel="stylesheet" href="../model/public/css/login_admin.css">
+    <link rel="stylesheet" href="model/public/css/login_admin.css">
     <!-- ========== TITTLE ========== -->
     <title>Document</title>
 </head>
