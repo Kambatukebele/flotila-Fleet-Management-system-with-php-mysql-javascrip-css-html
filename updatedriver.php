b12532f8a1ad5cd335d8b043ffb0f534d18bab4b
KAMBA  TUKEBELE Charles Junior <91629041+Kambatukebele@users.noreply.github.com>
2022-07-12 06:50:59 +0200

    Update updatedriver.php

--- updatedriver.php
+++ updatedriver.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '../controller/controller_updatedriver.php';
+    require_once 'controller/controller_updatedriver.php';
                 
 ?>
 <!DOCTYPE html>
@@ -15,9 +15,9 @@
      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
  
      <!-- =========== LINKING CSS ========== -->
-     <link rel="stylesheet" href="../model/public/css/reset.css">
-     <link rel="stylesheet" href="../model/public/css/style.css">
-     <link rel="stylesheet" href="../model/public/css/updatedriver.css">
+     <link rel="stylesheet" href="model/public/css/reset.css">
+     <link rel="stylesheet" href="model/public/css/style.css">
+     <link rel="stylesheet" href="model/public/css/updatedriver.css">
      <!-- ==========TITLE ========== -->
     <title>Update Driver</title>
 </head>
